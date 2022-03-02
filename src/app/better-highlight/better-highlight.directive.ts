03d2c1d5927550724c0afc86bae7057fa5cef759
Kumar Anand <kranand.vnit@gmail.com>
2022-03-02 08:50:26 +0530

    Use of HostBinding with Host Listner for attribute directives

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { ElementHelper } from 'protractor';
 
 @Directive({
@@ -8,19 +8,20 @@ export class BetterHighlightDirective implements OnInit {
 
   constructor(private _eleRef: ElementRef, private _renderer: Renderer2) { }
 
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
   ngOnInit(): void {
     // we are using host listner so we don't need setstyle in ngoninit description available in app.component.html file
     // this._renderer.setStyle(this._eleRef.nativeElement, 'background-color', 'grey');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this._renderer.setStyle(this._eleRef.nativeElement, 'background-color', 'grey');
+    // render code is commented as we are not using renderer instead we are using HostBinding But we need hostListner
+    // this._renderer.setStyle(this._eleRef.nativeElement, 'background-color', 'grey');
+    this.backgroundColor = 'grey' ;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this._renderer.setStyle(this._eleRef.nativeElement, 'background-color', 'transparent');
+    // this._renderer.setStyle(this._eleRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'cyan' ;
   }
-
-
-
 }
